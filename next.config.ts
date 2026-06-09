f141066d0f2b5c06b6d4cabb3beadfd0cb6cf7ac
Matheus Correia <matheuscorreia1910@gmail.com>
2026-06-09 14:53:42 -0300

    fix: remove static export config for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,30 @@
+// import type { NextConfig } from "next";
+
+// const isProd = process.env.NODE_ENV === "production";
+
+// const nextConfig: NextConfig = {
+//   output: "export",
+
+//   ...(isProd && {
+//     basePath: "/matheus-dev",
+//     assetPrefix: "/matheus-dev",
+//   }),
+
+//   images: {
+//     unoptimized: true,
+//   },
+
+//   devIndicators: false,
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export",
-
-  ...(isProd && {
-    basePath: "/matheus-dev",
-    assetPrefix: "/matheus-dev",
-  }),
-
   images: {
     unoptimized: true,
   },
-
   devIndicators: false,
 };
 
